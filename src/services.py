5cd6bfe9ad71e11ec56d2d91516bb6a0a1ba63c2
Gnomski <gritzzz@inbox.ru>
2025-06-03 18:40:26 +0300

    message

--- src/services.py
+++ src/services.py
@@ -6,7 +6,7 @@ async def send_to_backend(message):
     logging.info(f"[MOCK] would send to backend: {message}")
     async with aiohttp.ClientSession() as session:
         try:
-            async with session.post(BACKEND_URL, json={"message": message}) as resp:
+            async with session.post(BACKEND_URL, json= message) as resp:
                 if resp.status != 200:
                     logging.warning(f"Ошибка отправки: {resp.status}")
         except Exception as e:
